77369ef9533a3811b0984e76d97d8f3ebc7d69a6
Connor <connordbrown@gmail.com>
2023-07-07 16:09:53 -0600

    added button functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const button = document.getElementsByClassName("styled");
+button[0].addEventListener("click", (event) => {
+    alert("You clicked this button!");
+});
+button[1].addEventListener("click", (event) => {
+    alert("You clicked that button!");
+});
+button[2].addEventListener("click", (event) => {
+    alert("You clicked another button!");
+});
